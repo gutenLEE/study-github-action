b3faa4a0bfa37fd4e4516d45a79dec39de7114a7
gutenLee <3bnm1128@naver.com>
2023-09-27 11:42:11 +0900

    test run script file

---
+++ data/key.sh
@@ -0,0 +1,19 @@
+function makeSignature() {
+	nl=$'\\n'
+
+	TIMESTAMP=$(echo $(($(date +%s%N)/1000000)))
+	ACCESSKEY=${{ env.GITHUB_JOB}}
+    echo $ACCESSKEY
+	SECRETKEY="{secretKey}"				
+    
+	METHOD="GET"
+	URI="/photos/puppy.jpg?query1=&query2"
+
+	SIG="$METHOD"' '"$URI"${nl}
+	SIG+="$TIMESTAMP"${nl}
+	SIG+="$ACCESSKEY"
+
+	SIGNATURE=$(echo -n -e "$SIG"|iconv -t utf8 |openssl dgst -sha256 -hmac $SECRETKEY -binary|openssl enc -base64)
+
+    echo $SIGNATURE
+}
