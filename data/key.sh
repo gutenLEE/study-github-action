9e5a647e32a0df27a1f0c9835d31f93bc984242b
gutenLee <3bnm1128@naver.com>
2023-10-03 16:32:56 +0900

    if-else

--- data/key.sh
+++ data/key.sh
@@ -17,11 +17,19 @@ function makeSignature() {
 	SIGNATURE=$(echo -n -e "$SIG"|iconv -t utf8 |openssl dgst -sha256 -hmac $SECRETKEY -binary|openssl enc -base64)
 
 
-	curl -s 'https://ncloud.apigw.ntruss.com'$URI \
+	responseCode=$(curl -s 'https://ncloud.apigw.ntruss.com'$URI \
 		--header "Content-type:application/json" \
 		--header "x-ncp-apigw-timestamp:$TIMESTAMP" \
 		--header "x-ncp-iam-access-key:$ACCESSKEY" \
-		--header "x-ncp-apigw-signature-v2:$SIGNATURE" | sed -n 's/.*<returnCode>\(.*\)<\/returnCode>.*/\1/p'
+		--header "x-ncp-apigw-signature-v2:$SIGNATURE" | sed -n 's/.*<returnCode>\(.*\)<\/returnCode>.*/\1/p')
+	
+	if [responseCode==0];
+	then
+		echo ':rocket: Successfully added.'
+	else
+		echo ':rotating_light: failed.'
+	fi
+
 }
 
 makeSignature
